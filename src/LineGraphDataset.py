8b58f5337c6a15abea12f34fba2d8c488c234f85
kgb <bradykevin615@gmail.com>
2025-02-28 19:31:29 +0000

    Update LineGraphDataset to build graph with full data as edge attributes

--- src/LineGraphDataset.py
+++ src/LineGraphDataset.py
@@ -1,8 +1,8 @@
 import torch
 import numpy as np
+from itertools import repeat
 from torch_geometric.data import Dataset, Data
 from torch_geometric.transforms import LineGraph
-from src.NodeList import NodeList
 from src.utils import is_number
 import csv
 
@@ -13,51 +13,73 @@ class LineGraphDataset(Dataset):
         super().__init__(root, transform, pre_transform)
 
         self.graph_snapshots = []
-        self.nodes = NodeList()
+        self.nodes = {}
         self.edges = {}
-        self.ignore_features = ['src_ip', 'dst_ip']
         self.columns = columns
         self.transform = transform
 
         with open(csv_file_path, 'r') as csv_file:
+
             csv_reader = csv.DictReader(csv_file, fieldnames=list(self.columns.keys()))
+            next(csv_reader)   # Skip Header
+
             for row in csv_reader:
-                # Skip Header
-                if csv_reader.line_num == 1:
-                    continue
 
                 source = row['src_ip']
+                source_port = float(row['src_port'])
                 destination = row['dst_ip']
+                destination_port = float(row['dst_port'])
 
-                flow_edge = (self.nodes[source], self.nodes[destination])
-
-                if flow_edge[0] is None:
+                if self.nodes.get(source) is None:
                     key = len(self.nodes)
                     self.nodes.update({source: key})
-                    flow_edge = (key, flow_edge[1])
 
-                if flow_edge[1] is None:
+                if self.nodes.get(destination) is None:
                     key = len(self.nodes)
                     self.nodes.update({destination: key})
-                    flow_edge = (flow_edge[0], key)
 
-                self.edges[flow_edge] = [np.float32(x) for x in row.values() if is_number(x)]
+                flow_edge = (self.nodes.get(source), self.nodes.get(destination), source_port, destination_port)
+
+                #print(flow_edge)
+                #print(self.nodes.get_node_by_id(flow_edge[0]), self.nodes.get_node_by_id(flow_edge[1]))
+
+                row_values = [float(x) for x in row.values() if is_number(x)]
+
+                self.edges[flow_edge] = row_values
+                #{print(key, ':', value) for key, value in self.edges.items()}
+                #print('\n\n\n')
 
                 graph = Data(
-                    edge_index=torch.tensor(list(self.edges.keys()), dtype=torch.long).t().contiguous(),
-                    edge_attr=torch.tensor(list(self.edges.values()))
+                    edge_index=torch.tensor(
+                        [(a, b) for (a, b, c, d) in self.edges.keys()], dtype=torch.long
+                    ).t().contiguous(),
+                    edge_attr=torch.tensor([i for i in self.edges.values()])
                 )
 
-                graph.node_labels = self.nodes.keys()
-                #self.graph_snapshots.append(graph)
+                graph.node_lables = list(self.nodes.keys())
+                graph.num_nodes = len(graph.node_lables)
 
-                if csv_reader.line_num == 15:
+                '''
+                print(csv_reader.line_num)
+                print(graph.num_nodes)
+                print(graph.num_edges)
+                print('\n\n')
+                '''
+
+                self.graph_snapshots.append(graph)
+
+                
+                if csv_reader.line_num > 10000:
+                    #print(graph.edge_index)
+                    #print(graph.edge_attr)
+                    #print(graph.node_lables)
                     break
-            print('Nodes: ', len(self.nodes))
-            print('Edges: ', len(self.edges))
+
+            last_graph = self.graph_snapshots[-1]
+            print(last_graph)
+            print('Nodes: ', last_graph.num_nodes)
+            print('Edges: ', last_graph.num_edges)
             print('Graph Snapshots: ', len(self.graph_snapshots))
-
-
 
     def __len__(self):
         return len(self.graph_snapshots)
